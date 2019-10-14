8f4aebc53cf354e6567f500ae4e6126d6ac7aa9f
Tom Tijerina <ttijerina@wirediq.com>
2019-10-14 09:33:24 -0400

    Updated everything to use classes

--- src/components/TestPage.tsx
+++ src/components/TestPage.tsx
@@ -1,32 +1,45 @@
-import React from 'react';
-import { Route, RouteComponentProps } from "react-router-dom"
+import React from "react";
+import { Link, Route, RouteComponentProps } from "react-router-dom";
 
-const Page1: React.FC = () => {
-  return (
-    <p>1</p>
-  )
+class Page1 extends React.Component {
+  render = () => {
+    return <h1>Page 1</h1>;
+  };
 }
 
 //https://medium.com/@kaw2k/a-letter-of-appreciation-253ecab3f7d2
-interface Page2 extends RouteComponentProps<{ id: string, id2: string }> {};
-const Page2: React.FC<Page2> = ({ match } ) => {
-  return (
-    <div>
-      <p>2</p>
-      <p>Value: {match.params.id}</p>
-      <p> test: {match.params.id2} </p>
-    </div>
-  )
+
+interface Page2 extends RouteComponentProps<{ id: string; id2: string }> {}
+
+class Page2 extends React.Component<Page2> {
+  // constructor(props: Page2) {
+  //   super(props);
+  // }
+  render() {
+    console.log(this.props);
+    return (
+      <div>
+        <p>2</p>
+        <p> Value: {this.props.match.params.id}</p>
+        <p> test: {this.props.match.params.id2} </p>
+      </div>
+    );
+  }
 }
 
-const TestPage: React.FC = () => {
-  return (
-    <div>
-      <p>Test page</p>
-      <Route path="/test/page1" component={Page1} />
-      <Route path="/test/page2/:id/:id2" component={Page2} />
-    </div>
-  )
+export default class TestPage extends React.Component {
+  render = () => {
+    return (
+      <div>
+        <p>Test page</p>
+        <nav className="mainNav">
+          <h2>Test Nav</h2>
+          <Link to="/test/page1/">Test Page 1</Link>
+          <Link to="/test/page2/2/thisissomething">Test Page 2.2</Link>
+        </nav>
+        <Route path="/test/page1" component={Page1} />
+        <Route path="/test/page2/:id/:id2" component={Page2} />
+      </div>
+    );
+  };
 }
-
-export default TestPage
--- src/routers/AppRouter.tsx
+++ src/routers/AppRouter.tsx
@@ -1,67 +1,30 @@
-import React from 'react';
-import { RouteComponentProps, BrowserRouter, Route, Switch, Link } from "react-router-dom"
-import HomePage from "../components/HomePage"
-import TestPage from "../components/TestPage"
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-const Header: React.FC = () => {
-  return (
-    <div>
-      <Nav />
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from "../components/HeaderModule";
+import HomePage from "../components/HomePage";
+import TestPage from "../components/TestPage";
+
+
+class PageNotFound extends React.Component {
+  render = () => {
+    return <h1> Page not found!</h1>;
+  };
 }
 
-const PageNotFound: React.FC = () => {
-  return (<h1> Page not found!</h1>)
+export default class AppRouter extends React.Component {
+  render = () => {
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <Header />
+          <Switch>
+            <Route path="/" component={HomePage} exact={true} />
+            <Route path="/test" component={TestPage} />
+            <Route component={PageNotFound} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  };
 }
-
-const Nav: React.FC = () => {
-  return (<nav>
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/test">Test</Link>
-      </li>
-      <li>
-        <Link to="/test/page1/">Test Page 1</Link>
-      </li>
-      <li>
-        <Link to="/test/page2/2/thisissomething">Test Page 2.2</Link>
-      </li>
-    </ul>
-  </nav>
-  )
-}
-
-const AppRouter: React.FC = () => {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Header />
-        <Switch>
-          <Route path="/" component={HomePage} exact={true} />
-          <Route path="/test" component={TestPage} />
-          <Route component={PageNotFound} />
-        </Switch>
-      </BrowserRouter>
-    </div >
-  );
-}
-
-
-export default AppRouter;
