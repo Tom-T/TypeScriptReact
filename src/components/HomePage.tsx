9d3c4cf59899e7bf8632ca94d03f4227b6f1c701
Tom Tijerina <ttijerina@wirediq.com>
2019-10-15 09:20:37 -0400

    Added ability to toggle task on/off

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,20 +1,35 @@
 import React, { ReactPropTypes, SyntheticEvent } from "react";
 
 interface ItemState {
-  items: string[];
+  items: {
+    task: string;
+    completed: boolean;
+  }[];
 }
 
 interface ItemStateFunc extends ItemState {
   handleRemoveItem(e: SyntheticEvent): void;
+  handleCompletedToggle(e: SyntheticEvent): void;
 }
 
-class Item extends React.Component<{ handleRemoveItem(e: SyntheticEvent): void; itemText: string; itemIndex: number }> {
+class Item extends React.Component<{
+  handleRemoveItem(e: SyntheticEvent): void;
+  handleCompletedToggle(e: SyntheticEvent): void;
+  itemText: string;
+  itemCompleted: boolean;
+  itemIndex: number;
+}> {
   render() {
     return (
       <div>
         <p>{this.props.itemText}</p>
+        <p>{}</p>
         <button value={this.props.itemIndex} onClick={this.props.handleRemoveItem}>
-          Remove
+          {this.props.itemCompleted ? "Removed completed Task" : "Remove incomplete Task"}
+        </button>
+
+        <button value={this.props.itemIndex} onClick={this.props.handleCompletedToggle}>
+          {this.props.itemCompleted ? "Mark Incomplete" : "Mark Completed"}
         </button>
       </div>
     );
@@ -26,7 +41,14 @@ class Items extends React.Component<ItemStateFunc> {
     return (
       <div>
         {this.props.items.map((item, index) => (
-          <Item key={index} itemText={item} itemIndex={index} handleRemoveItem={this.props.handleRemoveItem} />
+          <Item
+            key={index}
+            itemText={item.task}
+            itemCompleted={item.completed}
+            itemIndex={index}
+            handleRemoveItem={this.props.handleRemoveItem}
+            handleCompletedToggle={this.props.handleCompletedToggle}
+          />
         ))}
       </div>
     );
@@ -39,21 +61,31 @@ export default class HomePage extends React.Component<{}, ItemState> {
     this.handleAddItem = this.handleAddItem.bind(this);
     this.handleRemoveItem = this.handleRemoveItem.bind(this);
     this.handleRemoveAll = this.handleRemoveAll.bind(this);
+    this.handleCompletedToggle = this.handleCompletedToggle.bind(this);
     this.state = {
       items: []
     };
   }
+  handleCompletedToggle(e: SyntheticEvent) {
+    e.preventDefault();
+      const thisIndex: number = parseInt(e.currentTarget.getAttribute("value") as string);
+
+    this.setState(state => {
+      let newState = this.state;
+      newState.items[thisIndex].completed = !this.state.items[thisIndex].completed;
+      return newState;
+    });
+  }
   handleRemoveItem(e: SyntheticEvent) {
     e.preventDefault();
-    let newSet: string[] = [];
-    newSet = this.state.items.filter((value: string, index: number) => {
-        return (index as number) !== parseInt(e.currentTarget.getAttribute("value") as string);
-      });
+    let newSet: { task: string; completed: boolean }[] = [];
+    newSet = this.state.items.filter((value: { task: string; completed: boolean }, index: number) => {
+      return (index as number) !== parseInt(e.currentTarget.getAttribute("value") as string);
+    });
 
-      this.setState(state => {
-        return { items: newSet };
-      });
-
+    this.setState(state => {
+      return { items: newSet };
+    });
   }
   handleRemoveAll(e: SyntheticEvent) {
     e.preventDefault();
@@ -75,7 +107,7 @@ export default class HomePage extends React.Component<{}, ItemState> {
     }
 
     this.setState(state => {
-      return { items: state.items.concat(newItem) };
+      return { items: state.items.concat({ task: newItem, completed: false }) };
     });
   }
   render = () => {
@@ -84,7 +116,11 @@ export default class HomePage extends React.Component<{}, ItemState> {
         <h1>Todo List</h1>
         <div>
           {/* <Items items={this.state.items} /> */}
-          <Items items={this.state.items} handleRemoveItem={this.handleRemoveItem} />
+          <Items
+            items={this.state.items}
+            handleRemoveItem={this.handleRemoveItem}
+            handleCompletedToggle={this.handleCompletedToggle}
+          />
         </div>
         <div>
           <form onSubmit={this.handleAddItem}>
