8d2ffb5a938ae3239e57cf25e764483acc4777ce
Tom <ham.glab@tomt.me>
2019-10-25 22:48:16 -0400

    Saving now. In process of adding a page to add, and then remove users

--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import { Route, RouteComponentProps } from "react-router-dom"
 
-const UserAdd: React.FC = () => {
-  return (
-    <p>To be done later</p>
-  )
+class UserAdd extends React.Component {
+  constructor(props: RouteComponentProps) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
 }
 
 //Displays user profile
--- src/obj/people.ts
+++ src/obj/people.ts
@@ -1,33 +1,53 @@
-export const test = "test"
-// import {BaseEntity, Entity, ObjectIdColumn, Column} from "typeorm";
+import { BaseEntity, Entity, ObjectIdColumn, Column } from "typeorm";
 
-// @Entity()
-// export class People extends BaseEntity {
+@Entity()
+export class People extends BaseEntity {
 
-//   @ObjectIdColumn()
-//   id: number | undefined;
+  @ObjectIdColumn()
+  id: number | undefined;
 
-//   @Column()
-//   firstName: string | undefined;
+  @Column()
+  firstName: string | undefined;
 
-//   @Column()
-//   lastName: string | undefined;
+  @Column()
+  lastName: string | undefined;
 
-//   @Column()
-//   email: string | undefined;
-  
-//   @Column()
-//   password: string | undefined;
+  @Column()
+  email: string | undefined;
 
-//   static findByName(firstName: string, lastName: string) {
-//     return this.createQueryBuilder("user")
-//         .where("user.firstName = :firstName", { firstName })
-//         .andWhere("user.lastName = :lastName", { lastName })
-//         .getMany();
-// }
-// }
-// @Entity()
-// export class User extends People{
+  @Column()
+  password: string | undefined;
+
+  static findByName(firstName: string, lastName: string) {
+    return this.createQueryBuilder("user")
+      .where("user.firstName = :firstName", { firstName })
+      .andWhere("user.lastName = :lastName", { lastName })
+      .getMany();
+  }
+  static findById(Id: number) {
+    return this.createQueryBuilder("user")
+      .where("user.id = :id", { Id })
+      .getOne();
+  }
+  static findByEmail(Email: string) {
+    return this.createQueryBuilder("user")
+      .where("user.email = :email", { Email })
+      .getOne();
+  }
+}
+@Entity()
+export class Owner extends People {
+  @Column()
+  Scanning: boolean | undefined; //If they do open/scan or not
 
 
-// }
+}
+@Entity()
+export class User extends People {
+
+  @Column()
+  Owner: number | undefined;  //Used to describe which homeowner this user belongs to.
+
+  @Column()
+  Box: string | undefined; //Optionally used to identify this user at the address. Eg "Unit 23", or "Apt J" (without Unit/Apt)
+}
